77c7717275197b1d57a82e7267d2e5f10b35d555
Mimansh707 <199878819+Mimansh707@users.noreply.github.com>
2026-05-24 11:26:44 -0500

    Connect frontend to render backend

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5001',
+  baseURL: process.env.REACT_APP_API_URL || 'http://127.0.0.1:5001',
 });
 
 export default API;
